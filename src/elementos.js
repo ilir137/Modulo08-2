db612a707da4344b8bc8a7569817825ddb75d8ff
ilir137 <appsilir@gmail.com>
2025-09-02 18:26:36 +0200

    Create elementos.js

---
+++ src/elementos.js
@@ -0,0 +1,10 @@
+export const listadoPacientesPediatria = document.getElementById("listadoPacientesPediatria");
+export const listadoPacientesPediatriaYMenoresDe10 = document.getElementById("listadoPacientesPediatriaYMenoresDe10");
+export const listadoPacientesProtocoloUrgencia = document.getElementById("listadoPacientesProtocoloUrgencia");
+export const listadoPacientesReasignadosAMedicoFamilia = document.getElementById("listadoPacientesReasignadosAMedicoFamilia");
+export const listadoPacientesMedicoFamilia = document.getElementById("listadoPacientesMedicoFamilia");
+export const divNumeroPacientesPorEspecialidad = document.getElementById("divNumeroPacientesPorEspecialidad");
+
+export const h2ProtocoloUrgencia = document.getElementById("h2ProtocoloUrgencia");
+
+export const spanHayPacientesPediatria = document.getElementById("spanHayPacientesPediatria");
